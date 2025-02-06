2e950b291ffe9f3fd16eff373d1130348bc2d0d9
Rafael Goulart <rafaelxms@hotmail.com>
2025-02-06 19:48:54 -0300

    Iniciando desafio

---
+++ src/test/java/com/goulart/api_agendamento_notificacao/ApiAgendamentoNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goulart.api_agendamento_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendamentoNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
